f6257020a1c439afaeceeeeadff28b692b107ce7
Matthew Steedman <steedman87@gmail.com>
2025-01-28 13:34:59 -0800

    mochi: More stuff

--- app/assets/javascripts/markdown.js
+++ app/assets/javascripts/markdown.js
@@ -115,18 +115,4 @@ $(() => {
     }
     $('#markdown-link-url').focus();
   });
-
-  QPixel.addPrePostValidation(text => {
-    // This regex catches Markdown images with no or default alt text.
-    const altRegex = /!\[(?:Image_alt_text)?\](?:\(.+(?!\\\))\)|\[.+(?!\\\])\])/gi;
-    if (text.match(altRegex)) {
-      const message = `It looks like you're posting an image with no alt text. Alt text is important for ` +
-                      `accessibility. Consider adding alt text to the images in your post - ` +
-                      `<a href="/help/alt-text">read this help article</a> for details and help writing alt text.`;
-      return [false, [{ type: 'warning', message }]];
-    }
-    else {
-      return [true, null];
-    }
-  });
 });
