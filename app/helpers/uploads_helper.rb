d32bcf04922ec4bb754cdb164a25cdc4ba1025a1
Matthew Steedman <steedman87@gmail.com>
2025-01-29 14:34:03 -0800

    heroku: Use digitalocean spaces URL

--- app/helpers/uploads_helper.rb
+++ app/helpers/uploads_helper.rb
@@ -3,7 +3,8 @@ module UploadsHelper
     bucket = Rails.cache.fetch 'active_storage/s3/bucket' do
       ActiveStorage::Blob.service.bucket.name
     end
-    "https://s3.amazonaws.com/#{bucket}/#{blob.is_a?(String) ? blob : blob.key}"
+    "https://#{bucket}.nyc3.cdn.digitaloceanspaces.com/#{blob.is_a?(String) ? blob : blob.key}"
+    # "https://s3.amazonaws.com/#{bucket}/#{blob.is_a?(String) ? blob : blob.key}"
   end
 
   def valid_image?(io)
