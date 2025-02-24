5b0bc0a24c957d777e35cfad97c3047683fb20ab
Matthew Steedman <steedman87@gmail.com>
2025-02-24 15:39:35 -0800

    mochi: Fix input height adjustment
    Now adjusts on load as well.

--- app/assets/javascripts/mochi/input_fields.js
+++ app/assets/javascripts/mochi/input_fields.js
@@ -1,4 +1,9 @@
 $(() => {
+  let $tgt = $('.js-comment-field, .js-post-field');
+  $tgt.each(function(i) {
+    this.style.height = 0;
+    this.style.height = this.scrollHeight + "px";
+  });
   $(document).on('input', '.js-comment-field, .js-post-field', async ev => {
     ev.target.style.height = 0;
     ev.target.style.height = ev.target.scrollHeight + "px";
